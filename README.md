bf3dc1687fdf0aab6e4a2ec47cb7f05088fa898b
Wouter Saaltink <wouter.saaltink@gmail.com>
2017-07-11 01:00:02 +0200

    Initial code working with cmake

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 gltf-viewer
+
+This project contains code from the tinygltf library by syoyo (https://github.com/syoyo/tinygltf). 
---
+++ impl.cc
@@ -0,0 +1,7 @@
+
+#define GLEXTL_IMPLEMENTATION
+#include <GL/glextl.h>
+
+#define TINYGLTF_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#include "tiny_gltf.h"
